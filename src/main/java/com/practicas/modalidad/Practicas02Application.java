11a2d9d0f803d57cb5e65f3a79c53d19fcd4774f
FrancoHub25 <francoretamozo25@gmail.com>
2023-09-13 00:13:45 -0500

    first

---
+++ src/main/java/com/practicas/modalidad/Practicas02Application.java
@@ -0,0 +1,13 @@
+package com.practicas.modalidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practicas02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practicas02Application.class, args);
+	}
+
+}
